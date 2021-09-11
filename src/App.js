35e71b3dc36e5872e75640ed365bfcf054636e91
Kan <kan.boorodom@gmail.com>
2021-09-11 13:21:23 +0700

    Add route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 /* API https://www.coingecko.com/en/api/documentation */
 import React from 'react'
-
 import './App.css';
 import Landing from './component/Landing/Landing';
 import Home from './component/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="coinapp">
-      <Landing />
-      {/* <Home /> */}
-    </div>
+    <Router>
+      <div className="coinapp">
+        <Switch>
+          <Route exact path = "/" render = {()=>{return <Landing />}}></Route>
+          <Route exact path = "/home" render = {()=>{return <Home />}}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/component/Landing/Header.js
+++ src/component/Landing/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 import './header.css'
 const Header = () => {
     return (
-        <header className = 'landing__header'>
-            <p className = 'landing__header--p'>Welcome to ...</p>
-            <h1 className = 'landing__header--header'>KBCrypto</h1>
-            <p className = 'landing__header--p'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, fuga?</p>
-            <button className = 'landing__header--btn'>Enter</button>
-        </header>
+            <header className = 'landing__header'>
+                <p className = 'landing__header--p'>Welcome to ...</p>
+                <h1 className = 'landing__header--header'>KBCrypto</h1>
+                <p className = 'landing__header--p'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, fuga?</p>
+                    <Link className = 'landing__header--btn' to = "/home">Enter website</Link>
+            </header>
     )
 }
 
